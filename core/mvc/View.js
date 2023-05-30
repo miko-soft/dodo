ba83c3a7554d842348c79bad0136620c4caf2cf8
Sasa M <smikodanic@gmail.com>
2023-05-30 15:27:50 +0200

    view

--- core/mvc/View.js
+++ core/mvc/View.js
@@ -11,20 +11,20 @@ class View extends Dd {
 
 
 
-  /************** VIEWS (HTML Content) ****************/
+  /************** VIEW (HTML) LOADERS ****************/
   /**
    * Parse elements with the dd-view attribute and load router views.
    * This method should be used in the controller's __loader() hook.
    * When 'sibling', 'prepend' and 'append' is used comment and text nodes will not be injected (only HTML elements (nodeType === 1)).
    * Example:
-   * <main dd-view="#main"></main> and in the controller await this.loadView('#main', 'pages/home/main.html', 'sibling');
+   * <main dd-view="#main"></main> and in the controller this.loadView('#main', 'pages/home/main.html', 'sibling');
    * @param {string} viewName - view name i.e. value of dd-view attribute, for example: '#home'
-   * @param {string} viewPath - view file path (relative to /view/ directory): 'pages/home/main.html'
+   * @param {string} viewContent - HTML content
    * @param {string} dest - destination where to place the view: inner, sibling, prepend, append
    * @param {string} cssSel - CSS selector to load part of the view file: 'div > p.bolded:nth-child(2)'
    * @returns {void}
    */
-  async loadView(viewName, viewPath, dest = 'inner', cssSel) {
+  loadView(viewName, viewContent = '', dest = 'inner', cssSel = '') {
     const attrName = 'dd-view';
 
     // empty content from the document by removing the dd-view-clone="" elements
@@ -32,21 +32,13 @@ class View extends Dd {
 
     // get a HTML element with dd-view attribute
     const elem = document.querySelector(`[${attrName}="${viewName}"]`);
-    this._debug('loadView', `--------- loadView dd-view="${viewName}" -- ${viewPath} ---------`, '#8B0892', '#EDA1F1');
+    this._debug('loadView', `--------- loadView dd-view="${viewName}" ---------`, '#8B0892', '#EDA1F1');
     if (!elem) { throw new Error(`Element dd-view="${viewName}" not found.`); }
-    if (!viewPath) { throw new Error(`View path for dd-view="${viewName}" is not defined.`); }
+    if (!viewContent) { throw new Error(`View content for dd-view="${viewName}" is not defined.`); }
 
-    const $viewsCached = window[this.$appName].$viewsCached;
-
-    // Get HTML content. First try from the cached JSON and if it doesn't exist then request from the server.
-    let htmlstr;
-    if (!!$viewsCached && !!$viewsCached[viewPath]) {
-      htmlstr = this._fetchCachedView(viewPath);
-      this._debug('loadView', `--from $viewsCached | htmlstr: ${htmlstr.length}`, '#8B0892');
-    } else {
-      htmlstr = await this._fetchRemoteView(viewPath);
-      this._debug('loadView', `--from server | htmlstr: ${htmlstr.length}`, '#8B0892');
-    }
+    // Get HTML content
+    let htmlstr = viewContent;
+    this._debug('loadView', `--htmlstr: ${htmlstr.length}`, '#8B0892');
 
     // apply CSS selector to get part of the view
     if (!!cssSel) {
@@ -80,26 +72,6 @@ class View extends Dd {
 
 
   /**
-   * Load multiple views asynchronously. Send resolved promise when all views are loaded.
-   * @param {any[][]} viewDefs - array of arrays: [[viewName, viewPath, dest, cssSel]]
-   * @returns {Promise<void[]>}
-   */
-  async loadViews(viewDefs) {
-    const promises = [];
-    for (const viewDef of viewDefs) {
-      const viewName = viewDef[0];
-      const viewPath = viewDef[1];
-      const dest = viewDef[2];
-      const cssSel = viewDef[3];
-      const promise = this.loadView(viewName, viewPath, dest, cssSel);
-      promises.push(promise);
-    }
-    return Promise.all(promises);
-  }
-
-
-
-  /**
    * Empty a view
    * @param {string} viewName - view name i.e. value of dd-view attribute, for example: '#home'
    * @param {string} dest - destination where the view was placed: inner, sibling, prepend, append
@@ -123,103 +95,53 @@ class View extends Dd {
 
 
 
-  /** HTML CONTENT FETCHERS **/
-  /**
-   * Fetch view from a cached file app/cache/views.json.
-   * @param {string} viewPath - view file path (relative to /view/ directory): 'pages/home/main.html'
-   * @returns {string} - string with HTML tags
-   */
-  _fetchCachedView(viewPath) {
-    const $viewsCached = window[this.$appName].$viewsCached;
-    const htmlstr = $viewsCached[viewPath];
-    return htmlstr;
-  }
-
-
-  /**
-   * Fetch view by sending a HTTP request to the server.
-   * @param {string} viewPath - view file path (relative to /view/ directory): 'pages/home/main.html'
-   * @returns {string} - string with HTML tags
-   */
-  async _fetchRemoteView(viewPath) {
-    const opts = {
-      encodeURI: true,
-      timeout: 21000,
-      retry: 0,
-      retryDelay: 1300,
-      maxRedirects: 0,
-      headers: {
-        'authorization': '',
-        'accept': '*/*', // 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-        'content-type': 'text/html; charset=UTF-8'
-      }
-    };
-    const viewHttpClient = new HTTPClient(opts);
-
-    const path = `/views/${viewPath}`; // /views/pages/home/main.html
-    const baseURIhost = `${window.location.protocol}//${window.location.host}`; // http://localhost:4400
-    const url = new URL(path, baseURIhost).toString(); // resolve the URL
-    const answer = await viewHttpClient.askHTML(url);
-    if (!answer) { throw new Error(`No answer for ${viewPath}`); }
-    if (answer.status !== 200) { throw new Error(`Status isn't 200 for ${viewPath}. HTTP Status: ${answer.status}`); }
-    if (!answer.res.content) { throw new Error(`Content is empty for ${viewPath}`); }
-
-    const htmlstr = answer.res.content;
-    return htmlstr;
-  }
-
-
-
   /************ JS LOADERS *********/
   /**
-   * Create <script> tags and execute js scripts.
-   * @param {string[]} urls - array of JS script URLs
-   * @param {number} waitMS - wait for miliseconds between the loading processes
-   * @param {object} opts - options {isModule:boolean, isDefer:boolean}
+   * Create <script> tags at the end of body and execute it.
+   * @param {string} url - JS script URL, https://code.jquery.com/jquery-3.7.0.min.js
+   * @param {object} opts - options {isModule:boolean, isDefer:boolean, isLazy:boolean}
    */
-  async lazyJS(urls, waitMS = 0, opts = {}) {
-    if (!urls) { return; }
-    for (const url of urls) {
-      await new Promise(r => setTimeout(r, waitMS));
+  loadJS(url, opts = {}) {
+    // remove the SCRIPT if already exists
+    this.unloadJS(url);
 
-      // check if SCRIPT already exists and if exists remove it
-      const elems = document.body.querySelectorAll(`script[src="${url}"]`);
-      if (elems.length) { this.unlazyJS([url]); }
+    // add the SCRIPT tag
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
 
-      // add the SCRIPT tag
-      const script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = url;
-      script.setAttribute('dd-lazyjs', '');
+    if (opts.isModule) { script.setAttribute('type', 'module'); }
+    if (opts.isDefer) { script.defer = true; }
+    if (opts.isLazy) { script.setAttribute('dd-lazyjs', ''); }
 
-      if (opts.isModule) script.setAttribute('type', 'module');
-      if (opts.isDefer) script.defer = true;
+    document.body.appendChild(script);
+  }
 
-      document.body.appendChild(script);
+
+  /**
+   * Remove SCRIPT tag with the specific url.
+   * @param {string} url - JS script URLs
+   */
+  unloadJS(url) {
+    const elems = document.body.querySelectorAll(`script[src="${url}"]`);
+    for (const elem of elems) {
+      if (!!elem) { elem.remove(); }
     }
   }
 
 
   /**
-   * Remove SCRIPT tag with dd-lazyjs attribute and the specific url.
-   * @param {string[] | undefined} urls - array of JS script URLs
+   * Remove all SCRIPT tags.
+   * @param {object} opts - options {isModule:boolean, isDefer:boolean, isLazy:boolean}
    */
-  unlazyJS(urls) {
-    if (!urls) { return; }
-    for (const url of urls) {
-      const elems = document.body.querySelectorAll(`script[src="${url}"][dd-lazyjs]`);
-      for (const elem of elems) {
-        if (!!elem) { elem.remove(); }
-      }
-    }
-  }
+  unloadAllJS(opts = {}) {
+    let css_selector = 'script';
+    if (opts.isModule) { css_selector += '[type="module"]'; }
+    if (opts.isDefer) { css_selector += '[defer]'; }
+    if (opts.isLazy) { css_selector += '[dd-lazyjs]'; }
 
+    const elems = document.querySelectorAll(css_selector) || [];
 
-  /**
-   * Remove all SCRIPT tags with the dd-lazyjs attribute.
-   */
-  unlazyAllJS() {
-    const elems = document.querySelectorAll('script[dd-lazyjs]') || [];
     for (const elem of elems) {
       if (!!elem) { elem.remove(); }
     }
@@ -243,10 +165,11 @@ class View extends Dd {
       const url = elem.getAttribute('src');
       const isModule = elem.getAttribute('type') === 'module';
       const isDefer = elem.hasAttribute('defer');
-      const opts = { isModule, isDefer };
+      const isLazy = elem.hasAttribute('dd-lazy');
+      const opts = { isModule, isDefer, isLazy };
 
-      this.unlazyJS([url]);
-      this.lazyJS([url], 30, opts);
+      this.unloadJS(url);
+      this.loadJS(url, opts);
 
       this._debug('ddLazyjs', `src="${url}" | opts: ${JSON.stringify(opts)}`, 'navy');
     }
@@ -255,43 +178,14 @@ class View extends Dd {
 
 
   /**
-   * Fetch js script content and evaluate it. The <script> tag will not be created like it is in lazyJS().
-   * This can work only for local files from the /assets/ folder due to CORS.
-   * @param {string} url - JS script URL, '/assets/js/test1.js'
+   * Fetch js script content and evaluate it. The <script> tag will not be created like it is in loadJS().
+   * This can work only for local files from the /public/ folder due to CORS.
+   * @param {string} jsContent - the content of the JS file. Use raw vite option: import jsContent from '/public/test.js?raw'
    */
-  async exeJS(url) {
-    if (!url) { return; }
-
-    // correct the URL
-    url = url.trim();
-    if (!/^http/.test(url)) {
-      const baseURIhost = `${window.location.protocol}//${window.location.host}`; // http://localhost:4400
-      url = new URL(url, baseURIhost).toString(); // resolve the URL
-    }
-
-    // default HTTP client
-    const opts = {
-      encodeURI: true,
-      timeout: 21000,
-      retry: 0,
-      retryDelay: 1300,
-      maxRedirects: 0,
-      headers: {
-        'authorization': '',
-        'accept': '*/*', // 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-        'content-type': 'text/html; charset=UTF-8'
-      }
-    };
-    const viewHttpClient = new HTTPClient(opts);
-
-    const answer = await viewHttpClient.askJS(url);
-    const jsContent = answer.res.content;
-
-    if (!!jsContent) {
-      const fja = new Function(`${jsContent};`);
-      fja();
-    }
-
+  async exeJS(jsContent = '') {
+    const AsyncFunction = Object.getPrototypeOf(async function () { }).constructor;
+    const fja = new AsyncFunction(`${jsContent};`);
+    await fja();
   }
 
 
@@ -299,48 +193,33 @@ class View extends Dd {
 
   /************ CSS LOADERS *********/
   /**
-   * Create <link rel="stylesheet"> tags and load CSS.
-   * Usually use it in the __loader() controller hook.
-   * @param {string|string[]} urls - one url, or array of CSS file URLs, ['/assets/css/common.css', '/assets/css/home.css']
+   * Create <link rel="stylesheet"> tag and load CSS.
+   * @param {string} url - CSS file URL, '/public/css/common.css'
    */
-  loadCSS(urls) {
-    if (!urls) { return; }
-    else if (!Array.isArray(urls)) { urls = [urls]; }
+  loadCSS(url) {
+    this.unloadCSS([url]);
 
-    for (const url of urls) {
-      // check if LINK tag already exists and if exists remove it
-      const elems = document.body.querySelectorAll(`link[href="${url}"]`);
-      if (elems.length) { this.unloadCSS([url]); }
-
-      // create LINK tag
-      const linkCSS = document.createElement('link');
-      linkCSS.setAttribute('rel', 'stylesheet');
-      linkCSS.setAttribute('href', url);
-      linkCSS.defer = true;
-      document.head.appendChild(linkCSS);
-    }
+    // create LINK tag
+    const linkCSS = document.createElement('link');
+    linkCSS.setAttribute('rel', 'stylesheet');
+    linkCSS.setAttribute('href', url);
+    linkCSS.defer = true;
+    document.head.appendChild(linkCSS);
   }
 
   /**
-   * Remove <link rel="stylesheet"> tags and unload CSS.
-   * Usually use it in the __loader() controller hook.
-   * @param {string|string[]} urls - array of CSS file URLs, ['/assets/css/common.css', '/assets/css/home.css'] or just ['/assets/css/'] to remove all folder files
+   * Remove <link rel="stylesheet"> tag and unload CSS.
+   * @param {string} url - CSS file URL, '/public/css/common.css'
    */
-  unloadCSS(urls) {
-    if (!urls) { return; }
-    else if (!Array.isArray(urls)) { urls = [urls]; }
-
-    for (const url of urls) {
-      const elems = document.head.querySelectorAll(`link[rel="stylesheet"][href="${url}"]`);
-      for (const elem of elems) {
-        if (!!elem) { elem.remove(); }
-      }
+  unloadCSS(url) {
+    const elems = document.head.querySelectorAll(`link[rel="stylesheet"][href="${url}"]`);
+    for (const elem of elems) {
+      if (!!elem) { elem.remove(); }
     }
   }
 
   /**
    * Append <style dd-ref="#reference"></style> tags in the <head>.
-   * Usually use it in the __loader() controller hook.
    * @param {string} cssRules - CSS rules, for example: div {font-weight: bold; color:red;}
    * @param {string} ref - reference which will be used for unplug
    */
@@ -405,42 +284,44 @@ class View extends Dd {
   }
 
 
-  /** EXPERIMENTAL
-   * Load the <head> tag content from the view file.
-   * @param {string} viewPath - view file path (relative to /view/ directory): 'pages/home/head.html'
-   * @param {string} dest - destination where to place the view: inner, append
+
+
+  /***** MISC *****/
+  /**
+   * Fetch HTML, CSS or JS content by sending a HTTP request to the server.
+   * @param {string} contentUrl - URL: http://localhost:4400/views/pages/home/main.html
+   * @returns {string} - the file content
    */
-  async loadHead(viewPath, dest = 'inner') {
-    // get the <head> HTML element
-    const elem = document.querySelector('head');
-    this._debug('loadHead', `--------- loadHead -- ${viewPath} ---------`, '#8B0892', '#EDA1F1');
-    if (this._debug().loadView) { console.log('elem::', elem); }
-    if (!elem) { throw new Error(`Element HEAD not found.`); }
-    if (!viewPath) { throw new Error(`View path is not defined.`); }
+  async grabFileContent(contentUrl = '') {
+    const opts = {
+      encodeURI: true,
+      timeout: 20000,
+      retry: 0,
+      retryDelay: 0,
+      maxRedirects: 0,
+      headers: {
+        'accept': '*/*'
+      }
+    };
+    const viewHttpClient = new HTTPClient(opts);
 
-    const $viewsCached = window[this.$appName].$viewsCached;
-
-    // Get HTML content. First try from the cached JSON and if it doesn't exist then request from the server.
-    let htmlstr;
-    if (!!$viewsCached && !!$viewsCached[viewPath]) {
-      htmlstr = this._fetchCachedView(viewPath);
-      this._debug('loadView', '--from $viewsCached', '#8B0892');
+    let url;
+    if (/^http/.test(contentUrl)) {
+      url = contentUrl;
     } else {
-      htmlstr = await this._fetchRemoteView(viewPath);
-      this._debug('loadView', '--from server', '#8B0892');
+      const baseURIhost = `${window.location.protocol}//${window.location.host}`; // http://localhost:4400
+      url = new URL(contentUrl, baseURIhost).toString(); // resolve the URL
     }
 
-    // remove previously generated elements, i.e. elements with the dd-headgen attribute
-    for (const clonedElem of document.querySelectorAll(`[dd-head-clone`)) { clonedElem.remove(); }
+    const answer = await viewHttpClient.askHTML(url);
+    if (!answer) { throw new Error(`No answer for ${url}`); }
+    if (answer.status !== 200) { throw new Error(`Status isn't 200 for ${url}. HTTP Status: ${answer.status}`); }
+    if (!answer.res.content) { throw new Error(`Content is empty for ${contentUrl}`); }
 
-    // load content in the element
-    if (dest === 'inner') {
-      elem.innerHTML = htmlstr;
+    let fileContent = answer.res.content;
+    fileContent = fileContent.replace('<script type="module" src="/@vite/client"></script>', '').trim();
 
-    } else if (dest === 'append') {
-      elem.insertAdjacentHTML('beforeend', htmlstr);
-      elem.lastElementChild.setAttribute('dd-head-clone', '');
-    }
+    return fileContent;
   }
 
 
@@ -464,8 +345,6 @@ class View extends Dd {
     const htmlString2 = wrapper.innerHTML; // modified htmlString
     return htmlString2;
   }
-
-
 
 
 
